5cb22531e6237a397b2984acdfe00ab570eb05d7
vlj2007 <vlj2007@yandex.ru>
2023-07-10 23:20:51 +0700

    I'm_created_a_new_branch_and_task2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1(){
         System.out.println("Задача 1");
@@ -14,5 +15,25 @@ public class Main {
     }
     public static void task2(){
         System.out.println("Задача 2");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Значение dog:" + dog);
+        dog = dog + 4;
+        System.out.println("Увеличим значение на 4 dog:" + dog);
+
+        System.out.println("Значение cat:" + cat);
+        cat = cat + 4;
+        System.out.println("Увеличим значение на 4 cat:" + cat);
+
+        System.out.println("Значение paper:" + paper);
+        paper = paper + 4;
+        System.out.println("Увеличим значение на 4 cat:" + cat);
+
     }
+
+    public static void task3(){
+        System.out.println("Задача 3");
+    }
+
 }
