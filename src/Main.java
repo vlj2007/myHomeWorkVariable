f13b5e7296a923a8d16d15bb906e78d5add5297a
vlj2007 <vlj2007@yandex.ru>
2023-07-10 20:51:34 +0700

    I'm_created_variables_and_methods_println

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
     }
     public static void task1(){
         System.out.println("Задача 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Значение dog:" + dog);
+        System.out.println("Значение cat:" + cat);
+        System.out.println("Значение paper:" + paper);
     }
     public static void task2(){
         System.out.println("Задача 2");
