1dd0ab21be983dcb6d40dbb3e57bf74cd2f0fd01
vlj2007 <vlj2007@yandex.ru>
2023-07-11 11:17:23 +0700

    I'm_created_a_new_task3_and_task4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
     public static void task1(){
         System.out.println("Задача 1");
@@ -28,12 +29,37 @@ public class Main {
 
         System.out.println("Значение paper:" + paper);
         paper = paper + 4;
-        System.out.println("Увеличим значение на 4 cat:" + cat);
+        System.out.println("Увеличим значение на 4 cat:" + paper);
 
     }
 
     public static void task3(){
         System.out.println("Задача 3");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Значение dog:" + dog);
+        dog = dog - 3.5;
+        System.out.println("Уменшение значение на 5,5 dog:" + dog);
+
+        System.out.println("Значение cat:" + cat);
+        cat = cat - 1.6;
+        System.out.println("Уменшение значение на 1.6 cat:" + cat);
+
+        System.out.println("Значение paper:" + paper);
+        paper = paper - 7639;
+        System.out.println("Уменшение значение на 7639 cat:" + paper);
+
+    }
+
+    public static void task4(){
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println("Значение friend:" + friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 
 }
